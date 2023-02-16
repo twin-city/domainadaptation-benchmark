20abf29d7d989d9f45d4d4ad28bda5a48a592117
RaphaelCouronne <raphael.couronne@gmail.com>
2023-02-16 11:26:50 +0100

    [FEAT] mini bench Cityscapes2Others output result table

--- src/segmentation/inference.py
+++ src/segmentation/inference.py
@@ -17,6 +17,7 @@ from mmcv.cnn.utils import revert_sync_batchnorm
 from mmseg.utils import build_dp
 from mmcv.runner import load_checkpoint
 import numpy as np
+import json
 
 def make_cfg_reproducible(cfg):
     cfg.seed = 0
@@ -81,17 +82,9 @@ def perform_inference(output_dir, train_configs_path, checkpoint_path, data_root
     # Process the metrics
     metric = test_dataset.evaluate(results, ["mIoU"])
     metric_dict = dict(config=train_configs_path, metric=metric)
+    mmcv.dump(metric_dict, osp.join(output_dir,"metrics.json"), indent=4)
 
-    CLASSES = ('road', 'sidewalk', 'building', 'pole', 'vegetation', 'person')
-
-    metric_dict_subset = {f"IoU.{class_name.lower()}": metric_dict["metric"][f"IoU.{class_name.lower()}"] for class_name in
-                         CLASSES}
-    metric_dict_subset.update({"mIoU": np.mean(list(metric_dict_subset.values()))})
-
-    mmcv.dump(metric_dict, "metrics.json", indent=4)
-    mmcv.dump(metric_dict_subset, "metrics_subset.json", indent=4)
-
-    return metric_dict, metric_dict_subset
+    return metric_dict
 
 
 
@@ -129,16 +122,63 @@ if __name__ == '__main__':
     training_dataset_type = "CityscapesDataset"
     checkpoint_path = osp.join(CHECKPOINT_DIR, 'semanticsegmentation/pspnet_r50-d8_512x1024_40k_cityscapes_20200605_003338-2966598c.pth')
     train_configs_path = '../mmsegmentation/pspnet_r50-d8_512x1024_40k_cityscapes.py'
+    test_datasets_types = ["TwincityUnrealDataset", "GTAVDataset"]
 
-    # For a pre-trained Cityscapes model
+    # Perform inference on twincity & GTAV
+    for dataset_type in test_datasets_types:
+        output_dir = f"../output_segmentation/workdir/{training_dataset_type}-2-{dataset_type}"
+        data_infos = get_infos_for_dataset(dataset_type)
+        perform_inference(output_dir, train_configs_path, checkpoint_path, *data_infos)
+
+    # Get the results and perform assessments
+    results = {}
+    for dataset_type in test_datasets_types:
+        output_dir = f"../output_segmentation/workdir/{training_dataset_type}-2-{dataset_type}"
+        json_path = osp.join(output_dir, "metrics.json")
+        with open(json_path) as jsonFile:
+            metrics = json.load(jsonFile)
+        results[dataset_type] = metrics
+
+    # Compute on a subset
+    show_metric = "IoU"
+    classes_subset = ('road', 'sidewalk', 'building', 'pole', 'vegetation', 'person')
+    for dataset_type in test_datasets_types:
+        metric_class_value_subset = [results[dataset_type]['metric'][f"{show_metric}.{c}"] for c in classes_subset]
+        metric_class_subset = [f"{show_metric}.{c}" for c in classes_subset]
+        metric_subset = np.mean(metric_class_value_subset)
+        results[dataset_type]['metric'].update({f"m{show_metric}_subset": metric_subset})
+        mmcv.dump(results[dataset_type], osp.join(output_dir,"metrics_subset.json"), indent=4)
+        #print(results[dataset_type]['metric']['mIoU'])
+        #print(results[dataset_type]['metric']['mIoU_subset'])
+
+    # Save results as a .csv, load if existing or create
+    import pandas as pd
+
+    csv_results_dir = osp.join("output", "benchmark")
+    mmcv.mkdir_or_exist(csv_results_dir)
+    csv_results_cityscapes2others = osp.join(csv_results_dir, "Cityscapes-2-Others.csv")
+    df_columns = ["mIoU", "mIoU_subset"]+metric_class_subset
+    try:
+        df = pd.read_csv(csv_results_cityscapes2others)
+        assert(df.columns) == df_columns
+    except:
+        df = pd.DataFrame(columns=df_columns)
+
+    # Set values in dataframe
+    for dataset_type in test_datasets_types:
+        row_index = f"{training_dataset_type.replace('Dataset','')}-2-{dataset_type.replace('Dataset','')}"
+        df = df.append(pd.DataFrame(results[dataset_type]['metric'], index=[row_index])[df.columns].round(3))
+
+    # Save dataframe
+    df.to_csv(csv_results_cityscapes2others)
+    with open("Cityscapes2Others.md", 'w') as md:
+        df.to_markdown(buf=md, tablefmt="grid")
+
+
+
+    """
+        # For a pre-trained Cityscapes model
     training_dataset_type = "TwincityUnrealDataset"
     checkpoint_path = osp.join(CHECKPOINT_DIR, '/home/raphael/work/code/domainadaptation-benchmark/work_dirs/legacy/TwincityUnreal_weight1-1000_loadedTruev2/latest.pth')
     train_configs_path = '../mmsegmentation/pspnet_r50-d8_512x1024_40k_cityscapes.py'
-
-
-    # Perform inference on twincity & GTAV
-    for dataset_type in ["TwincityUnrealDataset", "GTAVDataset"]:
-        output_dir = f"../output_segmentation/workdir/{training_dataset_type}-2-{dataset_type}"
-        data_infos = get_infos_for_dataset(dataset_type)
-        metric_dict, metric_dict_subset = perform_inference(output_dir, train_configs_path, checkpoint_path, *data_infos)
-        print(metric_dict_subset)
+    """
